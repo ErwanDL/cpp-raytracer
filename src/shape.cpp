fd2bb1f140040dab547375a4bd3c4ef431c3bf83
Erwan <erwan.delepinau@free.fr>
2021-04-23 16:55:51 +0200

    cleanup shape constructors

--- src/shape.cpp
+++ src/shape.cpp
@@ -1,16 +1,9 @@
-#include <cmath>
-#include <memory>
 #include <optional>
 
 #include "intersection.hpp"
 #include "ray.hpp"
 #include "shape.hpp"
 #include "utils.hpp"
-
-Shape::Shape(const Material& material) : material(material) {}
-
-Plane::Plane(const Point3& position, const Vector3& normal, const Material& material)
-    : Shape(material), position(position), normal(normal) {}
 
 std::optional<Intersection> Plane::intersect(const Ray& ray) const {
     float dDotN{normal.dot(ray.direction)};
@@ -29,11 +22,6 @@ std::optional<Intersection> Plane::intersect(const Ray& ray) const {
     Vector3 intersectionNormal = dDotN > 0.0f ? -normal : normal;
     return Intersection(intersectionLocation, intersectionNormal, t, this->material);
 }
-
-// CLASS SPHERE
-
-Sphere::Sphere(const Point3& centre, float radius, const Material& material)
-    : Shape(material), centre(centre), radius(radius) {}
 
 std::optional<Intersection> Sphere::intersect(const Ray& ray) const {
     // float a = raydirection.lengthSquared() is always equal to 1
--- src/shape.hpp
+++ src/shape.hpp
@@ -17,7 +17,7 @@ class Shape : public Intersectable {
   protected:
     Material material;
 
-    Shape(const Material& material);
+    Shape(const Material& material) : material(material) {}
 };
 
 class Plane : public Shape {
@@ -26,7 +26,8 @@ class Plane : public Shape {
     Vector3 normal;
 
   public:
-    Plane(const Point3& position, const Vector3& normal, const Material& material);
+    Plane(const Point3& position, const Vector3& normal, const Material& material)
+        : Shape(material), position(position), normal(normal) {}
 
     std::optional<Intersection> intersect(const Ray& ray) const override;
 };
@@ -37,7 +38,8 @@ class Sphere : public Shape {
     float radius;
 
   public:
-    Sphere(const Point3& centre, float radius, const Material& material);
+    Sphere(const Point3& centre, float radius, const Material& material)
+        : Shape(material), centre(centre), radius(radius) {}
 
     std::optional<Intersection> intersect(const Ray& ray) const override;
 };
